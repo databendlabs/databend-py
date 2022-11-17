2b4fe3ab49c9ed2a89b4dc64f0f3d6bccc89e846
hantmac <hantmac@outlook.com>
2022-11-17 12:15:04 +0800

    support data type reflect

---
+++ databend_py/datetypes.py
@@ -0,0 +1,39 @@
+import ast
+
+INTType = "int"
+FLOATTYPE = "float"
+DOUBLETYPE = "double"
+BOOLEANTYPE = "bool"
+ARRAYTYPE = "array"
+MAPTYPE = "map"
+JSONTYPE = "json"
+NULLTYPE = "null"
+
+
+class DatabendDataType:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def type_convert_fn(type_str: str):
+        if INTType in type_str.lower():
+            return int
+        elif FLOATTYPE in type_str.lower():
+            return float
+        elif DOUBLETYPE in type_str.lower():
+            return float
+        elif BOOLEANTYPE in type_str.lower():
+            return bool
+        elif MAPTYPE in type_str.lower():
+            return ast.literal_eval
+        elif ARRAYTYPE in type_str.lower():
+            return ast.literal_eval
+        elif JSONTYPE in type_str.lower():
+            return ast.literal_eval
+        else:
+            return str
+
+
+if __name__ == '__main__':
+    d = DatabendDataType()
+    print(d.type_convert_fn("string")('dkfjksj'))
--- databend_py/result.py
+++ databend_py/result.py
@@ -1,3 +1,7 @@
+import ast
+from .datetypes import DatabendDataType
+
+
 class QueryResult(object):
     """
     Stores query result from multiple response data.
@@ -9,17 +13,23 @@ class QueryResult(object):
         self.data_generator = data_generator
         self.with_column_types = with_column_types
         self.first_data = first_data
-        self.data = []
+        self.data_type_dict_list = []
         self.columns_with_types = []
+        self.type_convert = DatabendDataType.type_convert_fn
 
         super(QueryResult, self).__init__()
 
     def store(self, raw_data: dict):
-        self.data.append(raw_data.get("data"))
         fields = raw_data.get("schema")["fields"]
+        type_ls = []
+        datas = raw_data.get("data")
         for field in fields:
             column_type = (field['name'], field["data_type"]["type"])
+            type_ls.append(field["data_type"]["type"])
             self.columns_with_types.append(column_type)
+
+        for data in datas:
+            self.data_type_dict_list.append(dict(zip(data, type_ls)))
 
     def get_result(self):
         """
@@ -30,9 +40,11 @@ class QueryResult(object):
         for d in self.data_generator:
             self.store(d)
 
-        for rd in self.data:
-            for r in rd:
-                data.append(tuple(r))
+        for read_data in self.data_type_dict_list:
+            tmp_list = []
+            for d, t in read_data.items():
+                tmp_list.append(self.type_convert(t)(d))
+            data.append(tuple(tmp_list))
 
         if self.with_column_types:
             return self.columns_with_types, data
