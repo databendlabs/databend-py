e0fb70145456e4f3413deb733650eccd7870f38a
Li Yazhou <me.ssword@gmail.com>
2023-05-13 10:25:46 +0800

    tune comment

--- databend_py/connection.py
+++ databend_py/connection.py
@@ -122,6 +122,7 @@ class Connection(object):
                                  verify=True)
         try:
             resp_dict = json.loads(response.content)
+            # TODO(liyazhou): got KeyError here
             # self.client_session = resp_dict["session"]
             return resp_dict
         except Exception as err:
