82388b34475802225052f75fe524e122b16e13b9
hantmac <hantmac@outlook.com>
2023-05-24 16:08:47 +0800

    feat: release pypi workflow

--- databend_py/__init__.py
+++ databend_py/__init__.py
@@ -2,7 +2,4 @@ from .client import Client
 from .connection import Connection
 from .datetypes import DatabendDataType
 
-VERSION = (0, 4, 2)
-__version__ = '.'.join(str(x) for x in VERSION)
-
 __all__ = ['Client', 'Connection', 'DatabendDataType']
