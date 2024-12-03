3949022d50d0c83404fc3e538f90670b26d6c178
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-12-04 07:17:49 +0800

    ci: test_temp_table

--- tests/test_client.py
+++ tests/test_client.py
@@ -257,6 +257,14 @@ class DatabendPyTestCase(TestCase):
         _, data = client.execute("select 'False'::boolean union select 'True'::boolean")
         self.assertEqual(data, [(True,), (False,)])
 
+    def test_temp_table(self):
+        client = Client.from_url(self.databend_url)
+        client.execute("create temp table t1(a int)")
+        client.execute("insert into temp table t1 values (1)")
+        _, data = client.execute("select * from t1")
+        self.assertEqual(data, [(1,)])
+        client.execute("drop table t1")
+
 
 if __name__ == '__main__':
     print("start test......")
@@ -275,4 +283,5 @@ if __name__ == '__main__':
     dt.test_null_to_none()
     dt.tearDown()
     dt.test_cast_bool()
+    dt.test_temp_table()
     print("end test.....")
